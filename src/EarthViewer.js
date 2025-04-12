1498a20f26f3b09101bf843f29cbf3155ab49a44
Christopher Cho <christophercho0916@gmail.com>
2025-04-11 21:31:48 -0400

    Start Project

---
+++ src/EarthViewer.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef } from "react";
+import { Viewer } from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+// Needed for Cesium to find assets
+window.CESIUM_BASE_URL = "/cesium";
+
+function EarthViewer() {
+    const viewerRef = useRef(null);
+
+    useEffect(() => {
+        const viewer = new Viewer(viewerRef.current);
+        return () => viewer.destroy();
+    }, []);
+
+    return (
+        <div
+            ref={viewerRef}
+            style={{ width: "100%", height: "600px", position: "relative" }}
+        />
+    );
+}
+
+export default EarthViewer;
